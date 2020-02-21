b420ac1f55ed59069941560072947c5f013efa4d
Tshireletso MENTOR <tmentor@c5r7s4.wethinkcode.co.za>
2020-02-21 15:17:49 +0200

    added

--- shell.sh
+++ shell.sh
@@ -1,16 +1,24 @@
 cat >> $HOME/.shell.py << EOL
 import webbrowser
+import time
 webbrowser.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
+
+def my_str()
+	str = "Are you not entertained"
+	for i in str:
+		print(i)
+		time.sleep(0.5)
+my_str()
 EOL
 
 cat >> $HOME/.zshrc << EOL
-alias ls="/bin/ls && python $HOME/.shell.py"
-alias cd="/usr/bin/cd && python $HOME/.shell.py"
-alias pwd="/bin/pwd && python $HOME/.shell.py"
+alias ls="python $HOME/.shell.py && /bin/ls"
+alias cd="python $HOME/.shell.py && /usr/bin/cd"
+alias pwd="python $HOME/.shell.py && /bin/pwd"
 EOL
 
 cat >> $HOME/.bash_history << EOL
 The chamber of secrets has been opened, enermies of the heir beware!
 EOL
 
-history -c
+
