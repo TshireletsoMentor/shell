a7bbd7549cf78c346d4ba00cae8ae98b1a8b3468
Tshireletso MENTOR <tmentor@c5r7s4.wethinkcode.co.za>
2020-02-21 14:10:24 +0200

    add

--- shell.sh
+++ shell.sh
@@ -4,5 +4,6 @@ webbrowser.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
 EOL
 
 cat >> $HOME/.zshrc << EOL
-alias rabbit="echo run && python $HOME/.shell.py"
+alias ls="/bin/ls && python $HOME/.shell.py"
+alias cd="/bin/cd && python $HOME/.shell.py"
 EOL
